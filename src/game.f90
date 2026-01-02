2f385589c8ccc7da9d3fc738759ec41d27ce27e2
Zoey Sugerman-Brozan <zfs5725@rit.edu>
2026-01-02 15:12:14 -0500

    Created game module

---
+++ src/game.f90
@@ -0,0 +1,11 @@
+! Initialize the game state; output deck + visible and full game state arrays
+function initGame(nPlayers, nComps) result(deck, visState, hiddenState)
+        implicit none
+        
+        ! Initialize deck
+        integer, dimension(150) :: deck
+
+        ! Initialize game state arrays
+        integer, dimension(3, 4, n):: visState, hiddenState
+
+end function initGame
